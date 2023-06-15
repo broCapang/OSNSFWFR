b1f46e327dc0b59ef769f2a2efe2bdc2db8773ac
broCapang <U2000429@siswa.um.edu.my>
2023-06-15 15:00:24 +0800

    Done kot

--- Lab9/OS.c
+++ Lab9/OS.c
@@ -1,112 +1,80 @@
 #include <stdio.h>
 
-#define MAX_PHILOSOPHERS 10
+int tph, philname[20], status[20], howhung, hu[20], cho; 
 
-int philosophers[MAX_PHILOSOPHERS] = {0};  // Status of philosophers: 0 - thinking, 1 - hungry, 2 - eating
-int numHungry = 0;  // Number of hungry philosophers
-int hungryPositions[MAX_PHILOSOPHERS] = {0};  // Positions of hungry philosophers
-
-void promptUser() {
-    printf("Choose one of the following:\n");
-    printf("1. One can eat at a time.\n");
-    printf("2. Two can eat at a time.\n");
-    printf("3. Exit.\n");
+void one() 
+{ 
+  int pos=0, x, i; 
+  printf("\nAllow one philosopher to eat at any time\n"); 
+  for(i=0;i<howhung; i++, pos++) { 
+    printf("\nP %d is granted to eat", philname[hu[pos]]); 
+    for(x=pos;x<howhung;x++) 
+      printf("\nP %d is waiting", philname[hu[x]]); 
+  } 
 }
-
-void printStatus() {
-    for (int i = 0; i < numHungry; i++) {
-        int philosopher = hungryPositions[i];
-        switch (philosophers[philosopher]) {
-            case 0:
-                printf("P%d is thinking\n", philosopher);
-                break;
-            case 2:
-                printf("P%d is eating\n", philosopher);
-                break;
-            default:
-                printf("P%d is waiting\n", philosopher);
-                break;
+void two()
+{
+  int i, j, s = 0, t, r, x;
+  printf("\n Allow two philosophers to eat at the same time\n");
+  for (i = 0; i < howhung; i++)
+  {
+    for (j = i + 1; j < howhung; j++)
+    {
+      if (abs(hu[i] - hu[j]) != 1) // Check if the philosophers are not adjacent
+      {
+        printf("\nCombination %d ", (s + 1));
+        t = hu[i];
+        r = hu[j];
+        s++;
+        printf("\nP %d and P %d are granted to eat", philname[hu[i]], philname[hu[j]]);
+        for (x = 0; x < howhung; x++)
+        {
+          if ((hu[x] != t) && (hu[x] != r))
+            printf("\nP %d is waiting", philname[hu[x]]);
         }
+      }
     }
+  }
 }
 
-void allowEating(int p1, int p2) {
-    for (int i = 0; i < numHungry; i++) {
-        int philosopher = hungryPositions[i];
-        if (philosopher != p1 && philosopher != p2) {
-            philosophers[philosopher] = 0;  // Set non-eating philosophers to thinking
-        }
-    }
-}
+int main() 
+{
+  int i; 
 
-void handleOneAtATime() {
-    printf("One can eat at a time.\n");
-    for (int i = 0; i < numHungry; i++) {
-        int philosopher = hungryPositions[i];
-        philosophers[philosopher] = 2;  // Set the philosopher to eating
-        printStatus();
-        philosophers[philosopher] = 0;  // Set the philosopher to thinking after eating
-        printStatus();
-    }
-}
+    printf("\n\nDINING PHILOSOPHER PROBLEM"); 
+    printf("\nEnter the number of philosophers : "); 
+  scanf("%d",&tph); 
+  for(i=0;i<tph;i++) { 
+    philname[i] = (i+1); 
+    status[i]=1; 
+  } 
+  printf("How many are hungry : "); 
+  scanf("%d", &howhung); 
+  if(howhung==tph) { 
+    printf("\nAll are hungry..\nDead lock stage will occur"); 
+    printf("\nExiting.."); 
+  } 
+  else { 
+      for(i=0;i<howhung;i++) { 
+        printf("\nEnter the position of hungry philosopher %d: ", (i+1));
+        scanf("%d", &hu[i]);
+        hu[i]--; // Adjust the position by subtracting 1
+        status[hu[i]] = 2; 
+      }
+do { 
+        printf("\n1.One can eat at a time\t2.Two can eat at a time\t3.Exit\nEnter your choice:"); 
+        scanf("%d", &cho); 
+        switch(cho) { 
+          case 1: one(); 
+              break; 
+          case 2: two(); 
+              break; 
+          case 3:
+            printf("\nExiting.."); 
+            return 0; 
+          default: printf("\nInvalid option.."); 
+          } 
+        }while(1); 
+  } 
+} 
 
-void handleTwoAtATime() {
-    printf("Two can eat at a time.\n");
-    int combinations = 0;
-    for (int i = 0; i < numHungry - 1; i++) {
-        for (int j = i + 1; j < numHungry; j++) {
-            int p1 = hungryPositions[i];
-            int p2 = hungryPositions[j];
-            combinations++;
-            printf("Combination %d: P%d and P%d are eating. ", combinations, p1, p2);
-            allowEating(p1, p2);
-            printStatus();
-            allowEating(-1, -1);  // Reset the philosophers to thinking
-        }
-    }
-    printf("\n");
-}
-
-int main() {
-    int numPhilosophers;
-    printf("Enter the number of philosophers: ");
-    scanf("%d", &numPhilosophers);
-
-    printf("How many are hungry: ");
-    scanf("%d", &numHungry);
-
-    if (numHungry == numPhilosophers) {
-        printf("Error: All philosophers are hungry. Deadlock situation!\n");
-        return 0;
-    }
-
-    for (int i = 0; i < numHungry; i++) {
-        printf("Enter philosopher %d's position: ", i + 1);
-        scanf("%d", &hungryPositions[i]);
-        philosophers[hungryPositions[i]] = 1;  // Set hungry philosophers' status to 1
-    }
-
-    while (1) {
-        promptUser();
-
-        int choice;
-        scanf("%d", &choice);
-
-        switch (choice) {
-            case 1:
-                handleOneAtATime();
-                break;
-            case 2:
-                handleTwoAtATime();
-                break;
-            case 3:
-                printf("Exiting...\n");
-                return 0;
-            default:
-                printf("Invalid choice. Try again.\n");
-                break;
-        }
-    }
-
-    return 0;
-}
